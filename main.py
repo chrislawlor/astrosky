90f911436b336420947a759cba854cd13de2a080
Chris Lawlor <lawlor.chris@gmail.com>
2016-12-02 04:23:14 -0500

    Adds some sound effects, remaps controls

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Player(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = pygame.image.load(self.img)
         self.rect = pygame.rect.Rect((640, 650), self.image.get_size())
+        self.collide = pygame.mixer.Sound('sound/collide_1.wav')
+        self.laser_1 = pygame.mixer.Sound('sound/laser_1.wav')
+        self.laser_1_cooldown = 0.3
+        self.laser_1_cooldown_state = 0
         self.dx = 0
         self.dy = 0
 
@@ -30,31 +34,40 @@ class Player(pygame.sprite.Sprite):
     def thrust_backward(self, dt):
         self.dy += self.VERTICAL_MOVE
 
+    def shoot(self):
+        if self.laser_1_cooldown_state == 0:
+            self.laser_1.play()
+            self.laser_1_cooldown_state = self.laser_1_cooldown
+
     def update(self, dt, game):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
+        if keys[pygame.K_LEFT]:
             self.thrust_left(dt)
 
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT]:
             self.thrust_right(dt)
 
-        if keys[pygame.K_w]:
+        if keys[pygame.K_UP]:
             self.thrust_forward(dt)
 
-        if keys[pygame.K_s]:
+        if keys[pygame.K_DOWN]:
             self.thrust_backward(dt)
+
+        if keys[pygame.K_LSHIFT]:
+            self.shoot()
 
         self.rect.x += self.dx * dt
         self.rect.y += self.dy * dt
 
-        if self.rect.right > SCREEN_WIDTH and self.dx > 0:
-            self.dx = -self.dx + abs(self.dx / 2)
-
-        if self.rect.left < 0 and self.dx < 0:
-            self.dx = -self.dx - abs(self.dx / 2)
+        if (self.rect.right > SCREEN_WIDTH and self.dx > 0) or (self.rect.left < 0 and self.dx < 0):
+            if abs(self.dx) > 400:
+                self.collide.play()
+            self.dx = -(self.dx + (0 - self.dx / 2))
 
         if self.rect.top < 0 or self.rect.bottom > SCREEN_HEIGHT:
             self.dy = 0
+
+        self.laser_1_cooldown_state = max(self.laser_1_cooldown_state - dt, 0)
 
         if DEBUG:
             print("Player.dx = {}, dy = {}".format(self.dx, self.dy))
