3084d375fb6a9eba5ad4ff83bb3b91bc4034a0fb
Chris Lawlor <lawlor.chris@gmail.com>
2016-12-02 03:00:42 -0500

    Bounce off the walls

--- main.py
+++ main.py
@@ -3,23 +3,12 @@ import pygame
 SCREEN_HEIGHT = 800
 SCREEN_WIDTH = 1280
 FPS = 60
-
-
-class Sprite(pygame.sprite.Sprite):
-    image = None
-
-    def __init__(self, *groups):
-        super().__init__(*groups)
-        self._img = pygame.image.load(self.image)
-        self.rect = pygame
-
-    def draw(self, screen):
-        screen.blit(self._img, (self.x, self.y))
+DEBUG = False
 
 
 class Player(pygame.sprite.Sprite):
-    HORIZONTAL_MOVE = 150
-    VERTICAL_MOVE = 100
+    HORIZONTAL_MOVE = 15
+    VERTICAL_MOVE = 5
     img = 'art/ship.png'
 
     def __init__(self, *groups):
@@ -29,46 +18,46 @@ class Player(pygame.sprite.Sprite):
         self.dx = 0
         self.dy = 0
 
-    def move_left(self, dt):
-        self.dx = min(self.dx + 8, 300)
-        self.rect.x = max(self.rect.x - (self.HORIZONTAL_MOVE + self.dx) * dt, 0)
+    def thrust_left(self, dt):
+        self.dx -= self.HORIZONTAL_MOVE
 
-    def move_right(self, dt):
-        self.dx = max(self.dx - 8, -300)
-        self.rect.x = min(self.rect.x + (self.HORIZONTAL_MOVE - self.dx) * dt,
-                          SCREEN_WIDTH - self.rect.width)
+    def thrust_right(self, dt):
+        self.dx += self.HORIZONTAL_MOVE
 
-    def move_forward(self, dt):
-        self.dy = min(self.dy - 8, -80)
-        self.rect.y = max(self.rect.y - (self.VERTICAL_MOVE - self.dy) * dt, 0)
+    def thrust_forward(self, dt):
+        self.dy -= self.VERTICAL_MOVE
 
-    def move_backward(self, dt):
-        self.dy = max(self.dy + 8, 80)
-        self.rect.y = min(self.rect.y + (self.VERTICAL_MOVE + self.dy) * dt,
-                          SCREEN_HEIGHT - self.rect.height)
+    def thrust_backward(self, dt):
+        self.dy += self.VERTICAL_MOVE
 
     def update(self, dt, game):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.move_left(dt)
+            self.thrust_left(dt)
 
         if keys[pygame.K_d]:
-            self.move_right(dt)
+            self.thrust_right(dt)
 
         if keys[pygame.K_w]:
-            self.move_forward(dt)
+            self.thrust_forward(dt)
 
         if keys[pygame.K_s]:
-            self.move_backward(dt)
+            self.thrust_backward(dt)
 
+        self.rect.x += self.dx * dt
+        self.rect.y += self.dy * dt
 
-class Background(pygame.sprite.Sprite):
-    img = 'art/spacefield.png'
+        if self.rect.right > SCREEN_WIDTH and self.dx > 0:
+            self.dx = -self.dx + abs(self.dx / 2)
 
-    def __init__(self, *groups):
-        super().__init__(*groups)
-        self.image = pygame.image.load(self.img)
-        self.rect = pygame.rect.Rect((0, 0), self.image.get_size())
+        if self.rect.left < 0 and self.dx < 0:
+            self.dx = -self.dx - abs(self.dx / 2)
+
+        if self.rect.top < 0 or self.rect.bottom > SCREEN_HEIGHT:
+            self.dy = 0
+
+        if DEBUG:
+            print("Player.dx = {}, dy = {}".format(self.dx, self.dy))
 
 
 class Game(object):
@@ -78,7 +67,7 @@ class Game(object):
         sprites = pygame.sprite.Group()
 
         self.player = Player(sprites)
-        self.background = pygame.image.load('art/spacefield.png')
+        background = pygame.image.load('art/spacefield.png')
 
         while True:
             dt = clock.tick(FPS)
@@ -89,7 +78,7 @@ class Game(object):
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     return
 
-            screen.blit(self.background, (0, 0))
+            screen.blit(background, (0, 0))
             sprites.update(dt / 1000., self)
             sprites.draw(screen)
             pygame.display.flip()
