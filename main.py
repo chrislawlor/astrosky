1c18bd4f52360b2208ddb481a83be287ebf15bf3
Chris Lawlor <lawlor.chris@gmail.com>
2016-12-02 05:34:43 -0500

    Init the mixer with a smaller buffer size to reduce audio latency

--- main.py
+++ main.py
@@ -139,6 +139,7 @@ class Game(object):
 
 
 if __name__ == '__main__':
+    pygame.mixer.pre_init(buffer=1024)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game = Game()
