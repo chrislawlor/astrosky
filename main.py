43b9d95350fdcd1d6bd2bfeb3a4d0b118a8ec73c
Chris Lawlor <lawlor.chris@gmail.com>
2016-12-02 20:06:41 -0500

    Lasers! Enemies! Powerups!

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ SCREEN_WIDTH = 1000
 FPS = 60
 
 
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, start_position, *groups, dy=200):
+        super().__init__(*groups)
+        self.dy = dy
+        self.image = pygame.image.load('assets/ssr/PNG/Lasers/laserBlue07.png')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = start_position
+
+    def update(self, dt):
+        self.rect.y -= self.dy * dt
+        if self.rect.bottom < 0:
+            self.kill()
+
+
 class Player(pygame.sprite.Sprite):
     HORIZONTAL_MOVE = 15
     VERTICAL_MOVE = 5
@@ -39,10 +53,11 @@ class Player(pygame.sprite.Sprite):
     def thrust_backward(self, dt):
         self.dy += self.VERTICAL_MOVE
 
-    def shoot(self):
+    def shoot(self, game):
         if self.laser_1_cooldown_state == 0:
             self.laser_1.play()
             self.laser_1_cooldown_state = self.laser_1_cooldown
+            game.lasers.add(Laser(self.rect.midtop))
 
     def burst(self):
         """
@@ -71,7 +86,7 @@ class Player(pygame.sprite.Sprite):
             self.thrust_backward(dt)
 
         if keys[pygame.K_LSHIFT]:
-            self.shoot()
+            self.shoot(game)
 
         if keys[pygame.K_SPACE]:
             self.burst()
@@ -94,6 +109,25 @@ class Player(pygame.sprite.Sprite):
         # Adjust cooldowns
         self.laser_1_cooldown_state = max(self.laser_1_cooldown_state - dt, 0)
         self.burst_cooldown_state = max(self.burst_cooldown_state - dt, 0)
+
+
+class Enemy(pygame.sprite.Sprite):
+    points = 100
+    image_file = 'assets/ssr/PNG/Enemies/enemyBlack3.png'
+
+    def __init__(self, start_position, *groups, dy=50):
+        super().__init__(*groups)
+        self.dy = dy
+        self.image = pygame.image.load(self.image_file)
+        self.rect = pygame.rect.Rect(start_position, self.image.get_size())
+        self.explosion_sound = pygame.mixer.Sound('assets/sound/explosion_1.wav')
+
+    def update(self, dt):
+        self.rect.y += self.dy * dt
+
+    def destroy(self):
+        self.explosion_sound.play()
+        self.kill()
 
 
 class Star(object):
@@ -135,26 +169,44 @@ class Starfield(object):
 
 
 class Game(object):
+    def add_random_enemies(self):
+        for _ in range(randrange(1, 4)):
+            position = (randrange(0, (SCREEN_WIDTH - 70)), -100)
+            speed = randrange(90, 170, 10)
+            Enemy(position, self.enemies, dy=speed)
+
     def run(self, screen):
         stats = False
         clock = pygame.time.Clock()
+        player_score = 0
+        player_powerup = 0
 
         sprites = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
+        self.lasers = pygame.sprite.Group()
 
         self.player = Player((SCREEN_WIDTH / 2, 650), sprites)
+
         background = pygame.image.load('assets/art/spacefield1600x1000.png')
         starfield = Starfield(screen)
         show_starfield = True
+
         font = pygame.font.Font('assets/fonts/ShareTechMono-Regular.ttf', 16, bold=True)
+        score_font = pygame.font.Font('assets/ssr/Bonus/kenvector_future.ttf', 20)
 
         background_music = pygame.mixer.Sound('assets/sound/music/DigitalNativeLooped.ogg')
         background_music.set_volume(0.9)
+        powerup_sound = pygame.mixer.Sound('assets/sound/powerup_1.wav')
         background_music.play(loops=-1, fade_ms=2000)
 
         paused = False
+        enemy_cooldown = 4
+        enemy_cooldown_state = 0
+        enemy_cooldown_timer = 10  # how often we reduce the cooldown
+        enemy_cooldown_timer_state = 0
 
         while True:
-            dt = clock.tick(FPS)
+            dt = clock.tick(FPS) / 1000
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -176,15 +228,55 @@ class Game(object):
             if paused:
                 continue
 
+            # Spawn Enemies
+            if enemy_cooldown_state <= 0:
+                self.add_random_enemies()
+                enemy_cooldown_state = enemy_cooldown
+            if enemy_cooldown_timer_state <= 0:
+                enemy_cooldown_timer_state = enemy_cooldown_timer
+                # progressively faster spawn times
+                enemy_cooldown = max(enemy_cooldown - 0.2, 0.6)
+            enemy_cooldown_state -= dt
+            enemy_cooldown_timer_state -= dt
+
+            # Sprite Updates
+            self.lasers.update(dt)
+            sprites.update(dt, self)
+            self.enemies.update(dt)
+            self.lasers.update(dt)
+
+            # Draw
             screen.blit(background, (0, 0))
             if show_starfield:
-                starfield.update(dt / 1000)
-            sprites.update(dt / 1000., self)
+                starfield.update(dt)
+            self.enemies.draw(screen)
+            self.lasers.draw(screen)
             sprites.draw(screen)
+
+            # Game Logic
+            hits = pygame.sprite.groupcollide(self.enemies, self.lasers, False, True)
+            for enemy in hits.keys():
+                player_score += enemy.points
+                player_powerup += enemy.points
+                enemy.destroy()
+
+            if player_powerup > 5000:
+                powerup_sound.play()
+                self.player.laser_1_cooldown = max(0.2, self.player.laser_1_cooldown - 0.012)
+                player_powerup = 0
+
+            # Display
+            score_display = score_font.render(str(player_score), True, (200, 200, 200))
+            display_width = score_display.get_width()
+            display_x = SCREEN_WIDTH - 10 - display_width
+            screen.blit(score_display, (display_x, 10))
+
             if stats:
                 lines = [
                     "FPS {:.0f}".format(clock.get_fps()),
                     "Player dx={:+} dy={:+}".format(self.player.dx, self.player.dy),
+                    "Laser Atk Speed {:.3f}".format(self.player.laser_1_cooldown),
+                    "Wave Spawn Interval {:.3f}".format(enemy_cooldown),
                 ]
                 pixel_offset = 10
                 for line in lines:
